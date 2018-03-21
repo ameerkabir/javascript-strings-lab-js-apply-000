25d7e7b5c8c5a4933de69a6377478effe7ba7476
Ameer <ameerkabir2@gmail.com>
2018-03-21 11:56:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Hell0, " +  "nodejs!!";
 
-var greetSpecialGuest = `Hell0  ${specialGuest}  !`;
+var greetSpecialGuest = "Hell0"  +  "nodejs!!";
 
 var topic = `${space}`;
 
