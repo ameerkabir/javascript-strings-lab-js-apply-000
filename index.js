7d2a706092a7a00c26352adbc5e1cc8a888b055a
Ameer <ameerkabir2@gmail.com>
2018-03-21 11:44:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var greeting = "";
+var greeting = "Hello, everybody!";
 
-var specialGuest = "Neil deGrasse Tyson"
+var specialGuest = "Hell0, " +  "nodejs!!";
 
-var greetSpecialGuest = "" + specialGuest + "!";
+var greetSpecialGuest = `Hell0  ${specialGuest}  !`;
 
 var topic = "space";
 
